cb1aa2caed6771e3e34c84589ad36cbbdd8b4096
Ricardo <154460778+RicardoDCP09@users.noreply.github.com>
2025-06-17 18:36:09 -0400

    Page

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'ArepaTech C.A',
+  description: 'Created by ArepaTech C.A',
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon-180x180.png", sizes: "180x180", type: "image/png" }],
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
